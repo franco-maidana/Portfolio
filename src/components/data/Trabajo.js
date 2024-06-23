6d12db33de3c00c8e8ea2287b3587bf3c4defe14
Franco Maidana <francomaidana094@gmail.com>
2024-06-23 01:06:51 -0300

    se agrego ortro proyecto al portfolio

--- src/components/data/Trabajo.js
+++ src/components/data/Trabajo.js
@@ -6,6 +6,13 @@ const trabajos = [
     url: 'https://proyecto-final-react-maidana-2bdiy0ucl-franco-maidana.vercel.app/',
     tecnologias: 'HTML, CSS, JavaScript, React, Firebase',
     informacion: 'Maquetación de como sería un E-commerce'
+  },
+  {
+    id: '02',
+    nombre: 'Portfolio personal',
+    url: 'https://portfolio-ashen-gamma-52.vercel.app',
+    tecnologias: 'HTML, CSS, JavaScript, React',
+    informacion: 'Todos mis proyectos se van a ver volcados aca'
   }
   // Puedes añadir más trabajos aquí
 ];
