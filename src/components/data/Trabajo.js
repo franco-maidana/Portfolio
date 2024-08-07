c1f70f0d0a524b0839d4194045891622fc19d21e
Franco Maidana <francomaidana094@gmail.com>
2024-08-07 00:02:39 -0300

    se cambio una palabra que estaba mal puesta

--- src/components/data/Trabajo.js
+++ src/components/data/Trabajo.js
@@ -21,7 +21,7 @@ const trabajos = [
     urlBackend: 'https://github.com/franco-maidana/Backend-RedSocial',
     urlFront: 'https://github.com/franco-maidana/Frond-End-RedSocial-React',
     tecnologias: 'HTML, CSS, JavaScript, React, MongoDB, JWT, ',
-    informacion: `Este proyecto esta echo con el curso completo de Master en react de Udemy. para ver el trabajo e interar dentro de ella tendras que descargar el proyecto backend de forma local. Nota: es posible que tire error en vercel cuando se interactua mucho, borrar rutas de la url y se acomoda `
+    informacion: `Este proyecto esta echo con el curso completo de Master en react de Udemy. para ver el trabajo e interactuar dentro de ella tendras que descargar el proyecto backend de forma local. Nota: es posible que tire error en vercel cuando se interactua mucho, borrar rutas de la url y se acomoda `
   }
   // Puedes añadir más trabajos aquí
 ];
